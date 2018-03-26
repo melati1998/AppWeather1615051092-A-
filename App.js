b053ddebf8c441af804aa18780665e7b2df8ec49
melati1998 <37148092+melati1998@users.noreply.github.com>
2018-03-26 19:53:40 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import WeatherSingaraja from './src/WeatherSingaraja';
+
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <WeatherSingaraja />
+    );
+  }
+}
